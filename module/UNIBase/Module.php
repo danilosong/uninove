032f4ea6b48ca568b22fefa34540a99aac007f1e
danilosong <dan.coreia@hotmail.com>
2017-11-04 20:21:27 -0200

    - processo de criacao da camada no zend

--- module/UNIBase/Module.php
+++ module/UNIBase/Module.php
@@ -1,26 +1,14 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+/* @var  configuracao do módulo base
+ * @date 04-11-17
+ * @author Danilo Song <danilosong@outlook.com>
  */
+namespace UNIBase;
 
-namespace Application;
-
-use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
 class Module
 {
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
---
+++ module/UNIBase/config/module.config.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace UNIBase;
+
+return array();
